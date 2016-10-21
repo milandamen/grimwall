f1f2ae1c9d4e5dded0d4e80d97fd6d21bd8d5d37
joostvermeulen11 <joost.vermeulen11@gmail.com>
2016-10-21 03:20:23 -0700

    created AHero

---
+++ src/Domain/Heroes/AHero.cpp
@@ -0,0 +1,5 @@
+//
+// Created by joost on 10/21/16.
+//
+
+#include "AHero.h"
---
+++ src/Domain/Heroes/AHero.h
@@ -0,0 +1,14 @@
+//
+// Created by joost on 10/21/16.
+//
+
+#ifndef GRIMWALL_AHERO_H
+#define GRIMWALL_AHERO_H
+
+
+class AHero {
+
+};
+
+
+#endif //GRIMWALL_AHERO_H
