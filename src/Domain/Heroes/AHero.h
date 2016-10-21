8184e5bd218ff2cff923c02f00b9a2518fdbf4c5
joostvermeulen11 <joost.vermeulen11@gmail.com>
2016-10-21 03:20:46 -0700

    added AHero

--- src/Domain/Heroes/AHero.h
+++ src/Domain/Heroes/AHero.h
@@ -5,10 +5,28 @@
 #ifndef GRIMWALL_AHERO_H
 #define GRIMWALL_AHERO_H
 
+#include <iostream>
 
 class AHero {
-
+private:
+    virtual std::string name;
+    virtual std::string weapon;
+    virtual int reach;
+    virtual int moveSpeed;
+    virtual int attackDelay;
+    virtual int power;
+    virtual int hitPoints;
+public:
+    AHero(std::string name, std::string weapon, int reach, int moveSpeed, int attackDelay, int power, int hitPoints);
+    AHero();
+    ~AHero();
+    virtual std::string getName() = 0;
+    virtual std::string getWeapon() = 0;
+    virtual int getReach() = 0;
+    virtual int getMoveSpeed() = 0;
+    virtual int getAttackDelay() = 0;
+    virtual int getPower() = 0;
+    virtual int getHitPoints() = 0;
 };
-
 
 #endif //GRIMWALL_AHERO_H
