c0d0982e230936165976b0503c6f524cdd7537cf
Milan Damen <github@darvit.nl>
2016-11-15 21:12:13 +0100

    Start of Horwen

---
+++ src/Domain/Units/Heroes/Abilities/ShieldOfJustice.h
@@ -0,0 +1,14 @@
+//
+// Created by baksteen on 10/24/16.
+//
+
+#ifndef GRIMWALL_SHIELDOFJUSTICE_H
+#define GRIMWALL_SHIELDOFJUSTICE_H
+
+#include "../IAbility.h"
+
+class ShieldOfJustice : IAbility{
+
+};
+
+#endif //GRIMWALL_SHIELDOFJUSTICE_H
---
+++ src/Domain/Units/Heroes/Abilities/StrikeOfReflection.h
@@ -0,0 +1,16 @@
+//
+// Created by baksteen on 10/24/16.
+//
+
+#ifndef GRIMWALL_STRIKEOFREFLECTION_H
+#define GRIMWALL_STRIKEOFREFLECTION_H
+
+#include "../IAbility.h"
+
+class StrikeOfReflection : IAbility {
+
+};
+
+
+
+#endif //GRIMWALL_STRIKEOFREFLECTION_H
