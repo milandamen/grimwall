997d52ab8917d361a16efdf3a0c2c3480c939017
Jelte van Tartwijk <jlmtartw@avans.nl>
2016-11-24 13:05:07 +0100

    mouse input listener

--- src/Domain/FIFE/FIFEMouseListener.cpp
+++ src/Domain/FIFE/FIFEMouseListener.cpp
@@ -10,16 +10,32 @@ FIFEMouseListener::~FIFEMouseListener() {
     callbackMap.clear();
 }
 
-void FIFEMouseListener::mouseEntered(FIFE::MouseEvent& evt) {}
-void FIFEMouseListener::mouseExited(FIFE::MouseEvent& evt) {}
-void FIFEMouseListener::mousePressed(FIFE::MouseEvent& evt) {}
+void FIFEMouseListener::mouseEntered(FIFE::MouseEvent& evt) {
+    cout<< "Mouse Entered \n";
+}
+void FIFEMouseListener::mouseExited(FIFE::MouseEvent& evt) {
+    cout<< "Mouse Exited \n";
+}
+void FIFEMouseListener::mousePressed(FIFE::MouseEvent& evt) {
+    cout<< "Mouse Pressed \n";
+}
 
 void FIFEMouseListener::mouseReleased(FIFE::MouseEvent& evt) {
-    cout << "Hallo Milan \n";
+    cout << "Mouse Released \n";
 }
-void FIFEMouseListener::mouseClicked(FIFE::MouseEvent& evt) {}
-void FIFEMouseListener::mouseWheelMovedUp(FIFE::MouseEvent& evt) {}
-void FIFEMouseListener::mouseWheelMovedDown(FIFE::MouseEvent& evt) {}
-void FIFEMouseListener::mouseMoved(FIFE::MouseEvent& evt) {}
-void FIFEMouseListener::mouseDragged(FIFE::MouseEvent& evt) {}
+void FIFEMouseListener::mouseClicked(FIFE::MouseEvent& evt) {
+    cout<< "Mouse clicked \n";
+}
+void FIFEMouseListener::mouseWheelMovedUp(FIFE::MouseEvent& evt) {
+    cout<< "Scrolled up\n";
+}
+void FIFEMouseListener::mouseWheelMovedDown(FIFE::MouseEvent& evt) {
+    cout<< "Scrolled down \n";
+}
+void FIFEMouseListener::mouseMoved(FIFE::MouseEvent& evt) {
+//    cout<< "Mouse has moved \n";
+}
+void FIFEMouseListener::mouseDragged(FIFE::MouseEvent& evt) {
+    cout<< "Dragging the mouse \n";
+}
 
