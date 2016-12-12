7a0ceffe9c7495075cd19fded40dae16c9a19c0c
Jelte van Tartwijk <jlmtartw@avans.nl>
2016-12-12 13:29:53 +0100

    removed comments in KeyboardMapper.cpp

--- src/Input/KeyboardMapper.cpp
+++ src/Input/KeyboardMapper.cpp
@@ -7,18 +7,6 @@ KeyboardMapper::KeyboardMapper(IGame* game)
     EngineFacade::engine()->registerCallback("ESCAPE", new QuitCallback(game));
     
     // Camera
-    //EngineFacade::engine()->registerCallback("UP", new MoveCameraCallback(game, 1, "UP"));
-    //EngineFacade::engine()->registerCallback("RIGHT", new MoveCameraCallback(game, 1, "RIGHT"));
-    //EngineFacade::engine()->registerCallback("DOWN", new MoveCameraCallback(game, 1, "DOWN"));
-    //EngineFacade::engine()->registerCallback("LEFT", new MoveCameraCallback(game, 1, "LEFT"));
-    //EngineFacade::engine()->registerCallback("UP+RIGHT", new MoveCameraCallback(game, 1, "UPRIGHT"));
-    //EngineFacade::engine()->registerCallback("RIGHT+DOWN", new MoveCameraCallback(game, 1, "RIGHTDOWN"));
-    //EngineFacade::engine()->registerCallback("DOWN+LEFT", new MoveCameraCallback(game, 1, "DOWNLEFT"));
-    //EngineFacade::engine()->registerCallback("LEFT+UP", new MoveCameraCallback(game, 1, "LEFTUP"));
-    //EngineFacade::engine()->registerCallback("=", new ZoomCameraCallback(game, 5, "PLUS"));
-    //EngineFacade::engine()->registerCallback("-", new ZoomCameraCallback(game, 5, "MINUS"));
-
-
     EngineFacade::engine()->registerCallback("SPACE", new AttackCallback(game));
     EngineFacade::engine()->registerCallback("1", new UseAbilityCallback(game, 0));
     EngineFacade::engine()->registerCallback("2", new UseAbilityCallback(game, 1));
