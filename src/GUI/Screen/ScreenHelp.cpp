5de329bb930b023cc19db75a1ec77ba889542e1f
StephanRomer <sromer@avans.nl>
2017-01-16 11:21:40 +0100

    Help Screen

--- src/GUI/Screen/ScreenHelp.cpp
+++ src/GUI/Screen/ScreenHelp.cpp
@@ -8,10 +8,9 @@ ScreenHelp::ScreenHelp(IGame* game,AGUIManager* manager)
         : GUI(manager), game(game)
 {
 
-    this->imgBg = manager->addImage("assets/gui/heroSelectBackground.png", 0, 0, 1024, 786);
-    this->imgHelp = manager->addImage("assets/gui/exitMenuItem.png", 140, 300, 680, 100);
+    this->imgBg = manager->addImage("assets/gui/backgroundHelp.png", 0, 0, 1024, 786);
 
-    this->btnReturn = manager->addImageButton("", "assets/gui/settingReturn.png", 130, 550, 680, 100);
+    this->btnReturn = manager->addImageButton("", "assets/gui/settingReturn.png", 172, 650, 680, 100);
     this->btnReturn->setForegroundColor(255, 255, 255, 255);
     this->btnReturn->setBackgroundColor(0, 0, 0, 0);
     this->btnReturn->onClick([&]() {
