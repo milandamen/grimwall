88122783d6cebba221e25194040a1823fc74f3e1
StephanRomer <sromer@avans.nl>
2017-01-16 16:27:18 +0100

    Removed 'test' comment and fixed hero select

--- src/GUI/Screen/ScreenSelectHero.cpp
+++ src/GUI/Screen/ScreenSelectHero.cpp
@@ -8,21 +8,21 @@ ScreenSelectHero::ScreenSelectHero(IGame* game, AGUIManager* manager)
     this->imgBg = manager->addImage("assets/gui/heroSelectBackground.png", 0, 0, 1024, 786);
     this->imgChooseYourHero = manager->addImage("assets/gui/chooseYourHero.png", 140, 80, 680, 100);
 
-    this->btnDralas = manager->addImageButton("", "assets/gui/heroDralas.png", 304, 575, 50, 180);
+    this->btnDralas = manager->addImageButton("", "assets/gui/heroDralas.png", 50, 180, 304, 575);
     this->btnDralas->onClick([&]() {
         this->game->setHero(new Dralas());
         this->game->getHero()->getBase()->addAbility(new DeathStrike(this->game->getHero()));
         this->game->setUI("SelectLevel");
     });
 
-    this->btnHorwen = manager->addImageButton("", "assets/gui/heroHorwen.png", 304, 575, 385, 180);
+    this->btnHorwen = manager->addImageButton("", "assets/gui/heroHorwen.png", 375, 180, 304, 575);
     this->btnHorwen->onClick([&]() {
         std::cout << "Horwen does not exist, Dralas instead.";
         this->game->setHero(new Dralas());
         this->game->setUI("SelectLevel");
     });
-    
-    this->btnZarrku = manager->addImageButton("", "assets/gui/heroZarrku.png", 304, 575, 700, 180);
+
+    this->btnZarrku = manager->addImageButton("", "assets/gui/heroZarrku.png", 700, 180, 304, 575);
     this->btnZarrku->onClick([&]() {
         this->game->setHero(new Zarrku());
         this->game->setUI("SelectLevel");
